e337c863c7cd5fec982eee99746743325f4a7a3f
Laurel Chamberlin <lachambe@microsoft.com>
2023-05-08 14:15:57 -0700

    starter proj

---
+++ MqttFhl/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet;
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+// Create client options object
+MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+    .WithClientId("lachambe")
+    .WithTcpServer("localhost");
+ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+    .WithClientOptions(builder.Build())
+    .Build();
+
+// Set up handlers
+_mqttClient.ConnectedAsync += _mqttClient_ConnectedAsync;
+_mqttClient.DisconnectedAsync += _mqttClient_DisconnectedAsync;
+_mqttClient.ConnectingFailedAsync += _mqttClient_ConnectingFailedAsync;
+
+
+// Connect to the broker
+await _mqttClient.StartAsync(options);
+
+// Send a new message to the broker every second
+while (true)
+{
+    string json = JsonSerializer.Serialize(new { message = "Hi Mqtt", sent = DateTime.UtcNow });
+    await _mqttClient.EnqueueAsync("lachambe.ir/topic/json", json);
+
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
+
+Task _mqttClient_ConnectedAsync(MqttClientConnectedEventArgs arg)
+{
+    Console.WriteLine("Connected");
+    return Task.CompletedTask;
+};
+Task _mqttClient_DisconnectedAsync(MqttClientDisconnectedEventArgs arg)
+{
+    Console.WriteLine("Disconnected");
+    return Task.CompletedTask;
+};
+Task _mqttClient_ConnectingFailedAsync(ConnectingFailedEventArgs arg)
+{
+    Console.WriteLine("Connection failed check network or broker!");
+    return Task.CompletedTask;
+}
